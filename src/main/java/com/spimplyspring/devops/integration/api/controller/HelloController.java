e0cf7a3444bc4ed3058a7831856a2df9bc6c2ac0
unknown <sobhitakumarsahu@gmail.com>
2023-07-05 17:30:56 +0530

    second commit

--- src/main/java/com/spimplyspring/devops/integration/api/controller/HelloController.java
+++ src/main/java/com/spimplyspring/devops/integration/api/controller/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String greet(){
-        return "Hello, There!";
+        return "Hello, There! Welcome to Jenkins World";
     }
 }
